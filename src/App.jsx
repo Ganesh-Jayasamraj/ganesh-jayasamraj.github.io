a17f94a491e2a98ecef72e26d718c2bfa70dc575
Ganesh-Jayasamraj <ganesh.jayasamraj@gmail.com>
2023-06-01 06:50:49 +0530

    Tailwind css Test

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      Hello World
+    <div className='flex'>
+      <div className='fixed flex-none w-[20vw] h-[100vh] bg-orange-300 text-center'>test</div>
+      <div className='flex-1 w-[80vw] bg-green-700 ml-[20vw]'>
+        <div className='h-[100vh] text-center'>1</div>
+        <div className='h-[100vh] text-center'>2</div>
+        <div className='h-[100vh] text-center'>3</div>
+        <div className='h-[100vh] text-center'>4</div>
+      </div>
     </div>
   )
 }
