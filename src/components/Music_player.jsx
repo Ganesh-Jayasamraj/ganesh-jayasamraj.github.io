986e7125a80945c2c6f848087d8b4ac47d12bdf3
Ganesh-Jayasamraj <ganesh.jayasamraj@gmail.com>
2023-06-06 12:36:37 +0530

    Mini Music player on going

--- src/components/Music_player.jsx
+++ src/components/Music_player.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Music_player.css'
+import { BsFillPlayFill } from 'react-icons/Bs'
+import { BsFillPauseFill } from 'react-icons/Bs'
+import { BiSkipNext } from 'react-icons/bi'
+import { BiSkipPrevious } from 'react-icons/bi'
+import { BsArrowDownLeftCircle } from 'react-icons/Bs'
+import { BsArrowUpRightCircle } from 'react-icons/Bs'
+
+
 
 export const Music_player = () => {
+  const [Play, setPlay] = useState(true)
+  const [isExpand, setisExpand] = useState (true)
+
+  const toggleExpand = () => {
+    setisExpand(!isExpand)
+  }
+
+  const togglePlay = () => {
+    setPlay(!Play)
+  }
+
   return (
-    <div>
-        Music player
-        <audio controls autoPlay src='/Music/TheWeightOfLives.mp3' type="audio/mpeg" /> 
-    </div>
+      <div className='Mu_player'>
+        <button className='expand' onClick={toggleExpand}>
+          {isExpand ? <BsArrowDownLeftCircle /> : <BsArrowUpRightCircle />}
+        </button>
+          <div className='Mu_player_buttons'>
+            <button className='bi'><BiSkipPrevious /></button>
+            <button className='play' onClick={togglePlay}>
+              {Play ? <BsFillPlayFill />: <BsFillPauseFill />}
+            </button>
+            <button className='bi'><BiSkipNext /></button>
+          </div>
+          <div className='Mu_des'>
+            <p>Track_name</p>
+            <p>Author</p>
+          </div>
+      </div>
   )
 }
