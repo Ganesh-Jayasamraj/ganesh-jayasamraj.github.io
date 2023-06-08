31b93e62cc5bd026dc0440248d12f459db8e41f2
Ganesh-Jayasamraj <ganesh.jayasamraj@gmail.com>
2023-06-08 13:04:24 +0530

    another minor change in home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,8 +20,9 @@ export const Home = () => {
   return (
     <section id='#' className='h-[100vh] flex bg-blue-950'>
     {/* <section id='#' className='h-[100vh] flex'> */}
-      <div className='w-[50%] text-center place-self-center translate-y-[-5rem] text-cyan-100 inline-block'>
-        <h1 className=' text-[90px] my-[8rem] Scriptina_font text_skew ml-[-2rem] Neon_Effects'>Greetings Everyone</h1>
+      {/* <div className='w-[50%] text-center place-self-center translate-y-[-5rem] text-cyan-100 inline-block'> */}
+      <div className='w-[50%] text-center place-self-center text-cyan-100 inline-block'>
+        {/* <h1 className=' text-[90px] my-[8rem] Scriptina_font text_skew ml-[-2rem] Neon_Effects'>Greetings Everyone</h1> */}
         <br />
         <p className=' text-2xl text-cyan-300 inline-block lorem mx-40'> Got no idea what to write, So enjoy lorem <br /> <br /> "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe in quaerat, fuga iure iste, laborum libero nisi dicta illum quod deleniti ipsum aut repellat est eligendi ratione neque minus accusamus." </p>
         <br />
@@ -30,8 +31,10 @@ export const Home = () => {
         </a>
         <button id='CV' onClick={openCV} className=' px-5 py-3 mx-6 my-9 rounded-md bg-blue-400 text-white'> My Resume </button>
       </div>
+
       <div className='w-[45%] text-center place-self-center'>
-        <p className='text-[80px] Scriptina_font text_skew ml-[-2rem] text-cyan-100 Neon_Effects'>I'm Ganesh</p>
+        <h1 className=' text-[90px] mt-[-10rem] Scriptina_font text_skew text-cyan-100 Neon_Effects'>Greetings Everyone</h1>
+        <p className='text-[80px] Scriptina_font text_skew mt-[5rem] text-cyan-100 Neon_Effects'>I'm Ganesh</p>
       </div>
     </section>
   )
