4445a9ec43a3da580ad0f74c8ab9eff43ea17922
Ganesh-Jayasamraj <ganesh.jayasamraj@gmail.com>
2023-06-19 20:34:22 +0530

    Technical Skill section started

--- src/components/Learnings.jsx
+++ src/components/Learnings.jsx
@@ -6,9 +6,12 @@ import { SiJulia, SiRust } from 'react-icons/si'
 
 export const Learnings = () => {
   return (
-    <section id='learnings' className='lg:h-[100vh] sm:h-[100%]'>
-      <h1 className='text-center p-5 mt-6 mb-1 block text-[40px] lg:text-[50px] text-cyan-100 Neon_Effects'>Technical Skills</h1>
-      <div className='grid gap-4 lg:grid-cols-3 grid-rows-3 mx-20 grid-cols-1'>
+    <section id='learnings' className='lg:h-[100vh] h-[100%]'>
+      <h1 className='p-5 mt-6 mb-1 Neon_Effects Section_Heading'>Technical Skills</h1>
+
+    {/* For Laptops */}
+
+      <div className='lg:grid lg:gap-4 lg:grid-cols-3 lg:grid-rows-3 lg:mx-20 hidden'>
         <div className='card text-yellow-300'>
           <FaPython size={90}/>
           <br />
@@ -25,10 +28,8 @@ export const Learnings = () => {
           <br />
           React
         </div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        {/* <div className='card text-purple-400'>
+
+        <div className='card text-purple-400'>
           <SiJulia size={90}/>
           <br />
           Julia
@@ -37,9 +38,43 @@ export const Learnings = () => {
           <SiRust size={90}/>
           <br />
           Rust
-        </div> */}
+        </div>
 
       </div>
+
+    {/* For Mobile */}
+
+      <div className='grid grid-cols-2 lg:hidden mb-[6rem]'>
+        <div className='card_mb text-yellow-300'>
+          <FaPython size={55}/>
+          <br />
+          Python
+
+        </div>
+        <div className='card_mb text-yellow-500'>
+          <DiJavascript1 size={55}/>
+          <br />
+          JavaScript
+        </div>
+        <div className='card_mb text-cyan-400'>
+          <DiReact size={55}/>
+          <br />
+          React
+        </div>
+        <div className='card_mb text-purple-400'>
+          <SiJulia size={55}/>
+          <br />
+          Julia
+        </div>
+        <div className='card_mb text-red-500'>
+          <SiRust size={55}/>
+          <br />
+          Rust
+        </div>
+
+      </div>
+
+
     </section>
   )
 }
