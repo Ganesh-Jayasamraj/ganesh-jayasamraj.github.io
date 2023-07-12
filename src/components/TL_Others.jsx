ea18842321bdd6b1503985dc443159ab355ee186
GANESH JAYASAMRAJ <ganesh.jayasamraj@gmail.com>
2023-07-12 16:16:36 +0530

    TL Finished

--- src/components/TL_Others.jsx
+++ src/components/TL_Others.jsx
@@ -6,7 +6,7 @@ export const TL_Others = () => {
   return (
     <div className='text-white'>
       <div className='w-[100vw] ml-5 self-center'>
-        <h1 className='title_CODE p-4 Neon_Effects text-7xl'> &gt;&gt; Coding &lt;&lt; </h1>
+        <h1 className='title_CODE p-4 Neon_Effects text-7xl'> &gt;&gt; Other &lt;&lt; </h1>
         <div className="timeline_CODE">
           {
             // Coding_data.filter((item, i) => i < 4).map((Detail) => {
